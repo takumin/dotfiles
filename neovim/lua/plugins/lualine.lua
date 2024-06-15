f2a5c03f9c0a49078d9cb8773561046c1c0842ca
takumin <takumiiinn@gmail.com>
2024-06-15 13:10:12 +0900

    neovim: set event for lualine

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -1,7 +1,10 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    event = 'BufReadPost',
     opts = {
       options = {
         theme = 'iceberg_dark',
