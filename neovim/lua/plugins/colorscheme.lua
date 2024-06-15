c10f3b17ee811e9771ce143a6543921406797d79
takumin <takumiiinn@gmail.com>
2024-06-15 13:24:05 +0900

    neovim: update colorscheme and lualine config

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -1,11 +1,44 @@
+-- return {
+--   {
+--     'neozenith/estilo-xoria256',
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--       -- load the colorscheme here
+--       vim.cmd([[colorscheme xoria256]])
+--     end,
+--   },
+-- }
+
+-- return {
+--   {
+--     'sainnhe/everforest',
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--       -- load the colorscheme here
+--       vim.cmd([[colorscheme everforest]])
+--     end,
+--   },
+-- }
+
+-- return {
+--   {
+--     'cocopon/iceberg.vim',
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--       -- load the colorscheme here
+--       vim.cmd([[colorscheme iceberg]])
+--     end,
+--   },
+-- }
+
 return {
   {
-    'neozenith/estilo-xoria256',
+    'rmehri01/onenord.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme xoria256]])
-    end,
+    opts = {},
   },
 }
--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -7,7 +7,8 @@ return {
     event = 'BufReadPost',
     opts = {
       options = {
-        theme = 'iceberg_dark',
+        -- theme = 'iceberg_dark',
+        theme = 'onenord',
         icons_enabled = false,
         component_separators = {},
         section_separators = {},
