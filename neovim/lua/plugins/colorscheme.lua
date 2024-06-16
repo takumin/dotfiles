3ac9e4a96aa95cd95b221615e2c424f4954df902
takumin <takumiiinn@gmail.com>
2024-06-17 02:11:39 +0900

    neovim: removed colorscheme that are not compatible with treesitter

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -1,22 +1,9 @@
 -- return {
 --   {
---     'neozenith/estilo-xoria256',
---     lazy = false, -- make sure we load this during startup if it is your main colorscheme
---     priority = 1000, -- make sure to load this before all the other start plugins
---     config = function()
---       -- load the colorscheme here
---       vim.cmd([[colorscheme xoria256]])
---     end,
---   },
--- }
-
--- return {
---   {
 --     'sainnhe/everforest',
---     lazy = false, -- make sure we load this during startup if it is your main colorscheme
---     priority = 1000, -- make sure to load this before all the other start plugins
+--     lazy = false,
+--     priority = 1000,
 --     config = function()
---       -- load the colorscheme here
 --       vim.cmd([[colorscheme everforest]])
 --     end,
 --   },
@@ -24,21 +11,9 @@
 
 -- return {
 --   {
---     'cocopon/iceberg.vim',
---     lazy = false, -- make sure we load this during startup if it is your main colorscheme
---     priority = 1000, -- make sure to load this before all the other start plugins
---     config = function()
---       -- load the colorscheme here
---       vim.cmd([[colorscheme iceberg]])
---     end,
---   },
--- }
-
--- return {
---   {
 --     'rmehri01/onenord.nvim',
---     lazy = false, -- make sure we load this during startup if it is your main colorscheme
---     priority = 1000, -- make sure to load this before all the other start plugins
+--     lazy = false,
+--     priority = 1000,
 --     opts = {},
 --   },
 -- }
--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -7,9 +7,6 @@ return {
     event = 'UIEnter',
     opts = {
       options = {
-        -- theme = 'iceberg_dark',
-        -- theme = 'onenord',
-        theme = 'tokyonight',
         icons_enabled = false,
         component_separators = {},
         section_separators = {},
