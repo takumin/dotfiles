ed933a018e41ecfca3502e9bab45324175e44024
takumin <takumiiinn@gmail.com>
2024-06-17 01:59:30 +0900

    neovim: merge treesitter plugins

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -28,4 +28,12 @@ return {
       },
     },
   },
+  {
+    'windwp/nvim-autopairs',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    event = 'InsertEnter',
+    opts = {
+      check_ts = true,
+    },
+  },
 }
