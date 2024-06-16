23a9aa5698f51c3038525a41f70a4c3ded23b5c3
takumin <takumiiinn@gmail.com>
2024-06-17 02:29:47 +0900

    neovim: add navarasu/onedark.nvim colorscheme

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -21,6 +21,18 @@
 --   },
 -- }
 
+-- return {
+--   {
+--     'navarasu/onedark.nvim',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       require("onedark").setup({})
+--       vim.cmd([[colorscheme onedark]])
+--     end,
+--   },
+-- }
+
 return {
   {
     'folke/tokyonight.nvim',
