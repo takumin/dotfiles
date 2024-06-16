222ed5266bc50559907042cbc63cfa9ddbf7fed0
takumin <takumiiinn@gmail.com>
2024-06-17 02:40:17 +0900

    neovim: show relative path for lualine

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -11,6 +11,14 @@ return {
         component_separators = {},
         section_separators = {},
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          },
+        },
+      },
     },
   },
 }
