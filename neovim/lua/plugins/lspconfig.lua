9ba26eb9cf313343a4f9d16cf0511eb70c3630d0
Takumi Takahashi <takumiiinn@gmail.com>
2024-07-06 05:19:59 +0900

    feat(lspconfig): add terraform-ls setup and formatting

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -6,6 +6,15 @@ return {
       if vim.fn.executable('gopls') == 1 then
         require('lspconfig').gopls.setup({})
       end
+      if vim.fn.executable('terraform-ls') == 1 then
+        require('lspconfig').terraformls.setup({})
+        vim.api.nvim_create_autocmd({"BufWritePre"}, {
+          pattern = {"*.tf", "*.tfvars"},
+          callback = function()
+            vim.lsp.buf.format()
+          end,
+        })
+      end
     end,
   },
 }
