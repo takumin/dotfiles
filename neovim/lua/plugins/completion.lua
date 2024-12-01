bd1bb8ea56c606b6759108f08be083523a048869
Takumi Takahashi <takumiiinn@gmail.com>
2024-12-02 08:51:40 +0900

    feat(completion): replace LuaSnip with nvim-snippets

--- neovim/lua/plugins/completion.lua
+++ neovim/lua/plugins/completion.lua
@@ -1,12 +1,12 @@
 return {
 	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		build = "make install_jsregexp",
+		"garymjr/nvim-snippets",
 		dependencies = { "rafamadriz/friendly-snippets" },
 		event = { "BufRead", "BufNewFile" },
 		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
+			require("snippets").setup({
+				friendly_snippets = true,
+			})
 		end,
 	},
 	{
@@ -17,19 +17,16 @@ return {
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
+			"garymjr/nvim-snippets",
 			"rafamadriz/friendly-snippets",
 		},
 		event = { "BufRead", "BufNewFile" },
 		config = function()
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						-- luasnip.lsp_expand(args.body)
 						vim.snippet.expand(args.body)
 					end,
 				},
@@ -47,13 +44,9 @@ return {
 
 					["<CR>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							if luasnip.expandable() then
-								luasnip.expand()
-							else
-								cmp.confirm({
-									select = true,
-								})
-							end
+							cmp.confirm({
+								select = true,
+							})
 						else
 							fallback()
 						end
@@ -62,8 +55,6 @@ return {
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
-						elseif luasnip.locally_jumpable(1) then
-							luasnip.jump(1)
 						else
 							fallback()
 						end
@@ -72,8 +63,6 @@ return {
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
-						elseif luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
 						else
 							fallback()
 						end
@@ -81,8 +70,8 @@ return {
 				}),
 
 				sources = cmp.config.sources({
+					{ name = "snippets" },
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
 				}, {
 					{ name = "path" },
 				}, {
